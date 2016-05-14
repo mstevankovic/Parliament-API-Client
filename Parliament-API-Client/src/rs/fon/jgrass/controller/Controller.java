8cfeb6699f78306e1a2b70a679b68ebcfddf16b5
Marko Stevankovic <markostevankovic95@gmail.com>
2016-05-14 19:57:13 +0200

    Give up.

--- Parliament-API-Client/src/rs/fon/jgrass/controller/Controller.java
+++ Parliament-API-Client/src/rs/fon/jgrass/controller/Controller.java
@@ -1,9 +1,12 @@
 package rs.fon.jgrass.controller;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,7 @@ import rs.fon.jgrass.domain.Member;
 import rs.fon.jgrass.model.CollectionOfMembers;
 import rs.fon.jgrass.model.ParliamentAPICommunication;
 import rs.fon.jgrass.view.ParliamentAPIClientGUI;
+import rs.fon.jgrass.view.table_model.MemberTableModel;
 
 public class Controller {
 
@@ -57,13 +61,33 @@ public class Controller {
 	}
 
 	public static void fillTable() {
-		// TODO Auto-generated method stub
+		CollectionOfMembers instance = CollectionOfMembers.getInstance();
+		instance.getListOfMembers().clear();
 		
+		JsonArray membersJson = Controller.loadJsonMembersFromFile("data/serviceMembers.json");
+		
+		instance.getListOfMembers().addAll(Controller.parseMembers(membersJson));
+		
+		Controller.refreshTable();
+		
+		Controller.messageToStatusBar("Table filled with data loaded from service.");
 	}
 
 	public static void updateMembers() {
-		// TODO Auto-generated method stub
-		
+		MemberTableModel model = (MemberTableModel)ParliamentAPIClientGUI.getInstance().getTable().getModel();
+		if(model.getRowCount() <= 0){
+			Controller.showErrorDialogSetValueAt("Table is empty...");
+		} else {
+			CollectionOfMembers instance = CollectionOfMembers.getInstance();
+			instance.getListOfMembers().clear();
+			instance.getListOfMembers().addAll(model.getTableMembers());
+			try {
+				Controller.saveChanges();
+				Controller.messageToStatusBar("Updated fields saved.");
+			} catch (Exception e) {
+				Controller.showErrorDialog(e);
+			}
+		}
 	}
 
 	private static void loadMembersIntoCollectionOfMembers() {
@@ -123,10 +147,82 @@ public class Controller {
 		
 		return membersArray;
 	}
+	
+	private static List<Member> parseMembers(JsonArray membersJson) {
+		List<Member> members = new ArrayList<>();
+		
+		for(int i = 0; i < membersJson.size(); i++){
+			JsonObject memberJson = (JsonObject) membersJson.get(i);
+			
+			Member member = new Member();
+			member.setId(memberJson.get("id").getAsInt());
+			member.setFirstName(memberJson.get("name").getAsString());
+			member.setLastName(memberJson.get("lastName").getAsString());
+			
+			if(member.getBirthDate() != null){
+				try {
+					SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy.");
+					member.setBirthDate(sdf.parse(memberJson.get("birthDate").getAsString()));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			members.add(member);
+		}
+		
+		return members;
+	}
+
+	private static JsonArray loadJsonMembersFromFile(String path) {
+		try{
+			FileReader in = new FileReader(path);
+			Gson gson = new GsonBuilder().create();
+			
+			JsonArray membersJson = gson.fromJson(in, JsonArray.class);
+			
+			return membersJson;
+			
+		} catch(FileNotFoundException exc){
+			Controller.showErrorDialog(exc);
+		}
+		return null;
+	}
 
 	private static void messageToStatusBar(String message) {
 		ParliamentAPIClientGUI instance = ParliamentAPIClientGUI.getInstance();
 		instance.getTextArea().append(message + "\n");
+	}
+	
+	private static void refreshTable(){
+		MemberTableModel model = (MemberTableModel)ParliamentAPIClientGUI.getInstance().getTable().getModel();
+		model.fireTableDataChanged();
+	}
+	
+	public static void saveChanges() throws Exception {
+		JsonArray membersArray = new JsonArray();
+		List<Member> members = CollectionOfMembers.getInstance().getListOfMembers();
+		for (int i = 0; i < members.size(); i++) {
+			
+			Member m = members.get(i);
+
+			JsonObject memberJson = new JsonObject();
+			memberJson.addProperty("id", m.getId());
+			memberJson.addProperty("name", m.getFirstName());
+			memberJson.addProperty("lastName", m.getLastName());
+			if (m.getBirthDate() != null)
+				memberJson.addProperty("birthDate", new SimpleDateFormat("dd.MM.yyyy.").format(m.getBirthDate()));
+			membersArray.add(memberJson);
+		}
+
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("data/updatedMembers.json")));
+
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		String membersString = gson.toJson(membersArray);
+
+		out.println(membersString);
+		out.close();
 	}
 
 	public static void showAbouDialog() {
@@ -151,6 +247,14 @@ public class Controller {
 		JOptionPane.showMessageDialog(
 				null,
 				exc.getMessage(),
+				"ERROR", 
+				JOptionPane.ERROR_MESSAGE);
+	}
+
+	public static void showErrorDialogSetValueAt(String message) {
+		JOptionPane.showMessageDialog(
+				null,
+				message,
 				"ERROR", 
 				JOptionPane.ERROR_MESSAGE);
 	}
